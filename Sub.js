aa6bb07d80ae49877c076e17b6073f48333617db
Samruddhi Kalbhor <samruddhikalbhor19@gmail.com>
2026-04-26 16:46:37 +0530

    added add java and test files

--- Sub.js
+++
@@ -1,5 +0,0 @@
-function add(a, b) {
-  return a + b; // fixed BUG
-}
-
-console.log(add(2,3));
---
+++ src/test/java/addTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+public class addTest {
+    @Test
+    public void testForAdd() {
+        add add_var = new add();
+        Assert.assertEquals(7, add_var.addnumbers(4, 3));
+        Assert.assertEquals(-7, add_var.addnumbers(-4, -3));
+        Assert.assertEquals(201, add_var.addnumbers(201, 0));
+    }
+
+}
